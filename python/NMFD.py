29bb6ee43b31b6473854ba174a1c90031915d104
Shawn Wills <swills@csumb.edu>
2017-09-05 15:03:25 -0700

    Update variable names.

--- python/NMFD.py
+++ python/NMFD.py
@@ -10,50 +10,51 @@ class NMFD(object):
     DEBUG = True
     ITERFMT = 'Computing NMFD. iteration : {0:d}/{1:d}'
 
-    def __init__(self, v, r=3, t=10, i=50, w=None, h=None):
+    def __init__(self, v, rank=3, template=10, iterations=50, w=None, h=None):
         """
         """
-	# input spectrogram or scalogram
+        # input spectrogram or scalogram
         self.v = v
-	# data size
+        # data size
         self.m, self.n = self.v.shape
-	# number of templates
-        self.r = r
-	# template size
-        self.t = t
-	# number or iterations
-        self.i = i
+        # number of templates
+        self.rank = rank
+        # template size
+        self.template = template
+        # number or iterations
+        self.iterations = iterations
 
-	# bases
+        # bases
         if w is not None:
             self.w = w
-            self.r = self.w.shape[1]
-            self.t = self.w.shape[2]
+            self.rank, self.template = self.w.shape[1:3]
         else:
-            self.w = np.random.rand(self.m, self.r, self.t)
+            self.w = np.random.rand(self.m, self.rank, self.template)
 
-	# weights
+        # weights
         if h is not None:
             self.h = h
-            self.r = self.h.shape[0]
+            self.rank = self.h.shape[0]
         else:
-            self.h = np.random.rand(self.r, self.n)
+            self.h = np.random.rand(self.rank, self.n)
 
+        self.cost = np.zeros(self.iterations)
+
+        # instance helper matrices
         self.one = np.ones((self.m, self.n))
-        self._lambda = np.zeros((self.m, self.n))
-        self._vonlambda = None
-        self.cost = np.zeros(self.i)
-
-        self.constraint = 1.02**np.arange(0, self.t) - 1.0
+        self.constraint = 1.02**np.arange(0, self.template) - 1.0
         self.constraint[0:2] = 0.0
 
     def compute_lambda(self):
         """
         computation of Lambda
         """
-        self._lambda[:] = 0.0
+        if self._lambda is None:
+            self._lambda = np.zeros((self.m, self.n))
+        else:
+            self._lambda[:] = 0.0
         for m in xrange(self.m):
-            for r in xrange(self.r):
+            for r in xrange(self.rank):
                 cv = np.convolve(self.w[m, r, :], self.h[r, :])
                 self._lambda[m, :] += cv[0:self._lambda.shape[1]]
 
@@ -75,15 +76,15 @@ class NMFD(object):
         """
         average along t
         """
-        hu = np.zeros((self.r, self.n))
-        hd = np.zeros((self.r, self.n))
-        for r in xrange(self.r):
+        hu = np.zeros((self.rank, self.n))
+        hd = np.zeros((self.rank, self.n))
+        for r in xrange(self.rank):
             for m in xrange(self.m):
                 cv = np.convolve(self._vonlambda[m, :],
                                  np.flipud(self.w[m, r, :]))
-                hu[r, :] += cv[self.t-1:self.t+self.n-1]
+                hu[r, :] += cv[self.template-1:self.template+self.n-1]
                 cv = np.convolve(self.one[m, :], np.flipud(self.w[m, r, :]))
-                hd[r, :] += cv[self.t-1:self.t+self.n-1]
+                hd[r, :] += cv[self.template-1:self.template+self.n-1]
 
         # average along t
         self.h *= hu/hd
@@ -92,7 +93,7 @@ class NMFD(object):
         """
         update of Wt
         """
-        for t in xrange(self.t):
+        for t in xrange(self.template):
             shift_h = self.row_shift(self.h, t)
             self.w[:, :, t] *= (np.dot(self._vonlambda, shift_h) /
                                 (np.dot(self.one, shift_h) + self.EPS
@@ -111,7 +112,7 @@ class NMFD(object):
 
     def nmfd(self):
         """
-         NMFD(v, r=3, t=10, i=50, init_W=None, init_H=None)
+         NMFD(v, rank=3, template=10, iterations=50, w=None, h=None)
             NMFD as proposed by Smaragdis (Non-negative Matrix Factor
             Deconvolution; Extraction of Multiple Sound Sources from Monophonic
             Inputs). KL divergence minimization. The proposed algorithm was
@@ -119,21 +120,21 @@ class NMFD(object):
         Input :
            - v : array_like
                 magnitude spectrogram to factorize (is a MxN numpy array)
-           - r : int
+           - rank : int
                 number of templates (unused if init_W or init_H is set)
-           - t : int
-                template size (in number of frames in the spectrogram) (unused if
-                init_W is set)
-           - i :
+           - template : int
+                template size (in number of frames in the spectrogram) (unused
+                if init_W is set)
+           - iterations :
                 number of iterations
-           - init_W :
+           - w :
                 initial value for W.
-           - init_H :
+           - h :
                 initial value for H.
         Output :
-           - W : ndarray
+           - w : ndarray
                 time/frequency template (MxRxT array, each template is TxM)
-           - H : ndarry
+           - h : ndarry
                 activities for each template (RxN array)
 
          Copyright (C) 2015 Romain Hennequin
@@ -144,9 +145,9 @@ class NMFD(object):
          W : MxRxT
         """
 
-        for i in xrange(self.i):
+        for i in xrange(self.iterations):
             if self.DEBUG:
-                warnings.warn(self.ITERFMT.format(i+1, self.i))
+                warnings.warn(self.ITERFMT.format(i+1, self.iterations))
             self.nmfd_iter(i)
 
             return (self.w, self.h, self.cost)
